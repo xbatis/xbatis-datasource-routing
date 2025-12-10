cf47fe022be92b0e50d6e2fc318c0f14f9dc8693
Ai东 <abc-127@live.cn>
2025-12-10 15:39:35 +0800

    优化

--- src/main/java/cn/xbatis/datasource/routing/DataSourceHolder.java
+++ src/main/java/cn/xbatis/datasource/routing/DataSourceHolder.java
@@ -16,15 +16,29 @@ package cn.xbatis.datasource.routing;
 
 import java.util.ArrayDeque;
 import java.util.Deque;
+import java.util.concurrent.Callable;
 
+/**
+ * 数据源挂载
+ */
 public class DataSourceHolder {
 
     private static final ThreadLocal<Deque<String>> TL = ThreadLocal.withInitial(ArrayDeque::new);
 
+    /**
+     * 追加/切换数据源
+     * 记住别忘记remove
+     *
+     * @param type 数据源分类
+     */
     public static void add(String type) {
         TL.get().addLast(type);
     }
 
+    /**
+     * 获取当前数据源
+     * @return 数据源分类
+     */
     public static String getCurrent() {
         Deque<String> list = TL.get();
         if (list.isEmpty()) {
@@ -33,7 +47,44 @@ public class DataSourceHolder {
         return list.getLast();
     }
 
+    /**
+     * 移除最近追加/切换的数据源
+     */
     public static void remove() {
         TL.get().removeLast();
+    }
+
+    /**
+     * 在指定数据库下执行
+     *
+     * @param type     数据源分类
+     * @param runnable 执行器
+     */
+    public static void execute(String type, Runnable runnable) {
+        try {
+            add(type);
+            runnable.run();
+        } finally {
+            remove();
+        }
+    }
+
+    /**
+     * 在指定数据源下执行
+     *
+     * @param type     数据源分类
+     * @param callable 执行器
+     * @param <V>      返回值类型
+     * @return callable 执行后的返回值
+     */
+    public static <V> V execute(String type, Callable<V> callable) {
+        try {
+            add(type);
+            return callable.call();
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            remove();
+        }
     }
 }
